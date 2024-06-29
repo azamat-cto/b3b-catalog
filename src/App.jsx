94fd7d9eaac32e4e4ce63125f415881533ba43b9
azamatcto <azamatkhabibullaevcto@gmail.com>
2024-06-29 22:18:46 +0500

    chore: add products

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,378 @@
+import Image01 from "./assets/images/01.jpg";
+import Image02 from "./assets/images/02.jpg";
+import Image03 from "./assets/images/03.jpg";
+import Image04 from "./assets/images/04.jpg";
+import Image05 from "./assets/images/05.jpg";
+import Image06 from "./assets/images/06.jpg";
+import Image07 from "./assets/images/07.jpg";
+import Image08 from "./assets/images/08.jpg";
+import Image09 from "./assets/images/09.jpg";
+import Image10 from "./assets/images/10.jpg";
+import Image11 from "./assets/images/11.jpg";
+import Image12 from "./assets/images/12.jpg";
+import Image13 from "./assets/images/13.jpg";
+import Image14 from "./assets/images/14.jpg";
+import Image15 from "./assets/images/15.jpg";
+import Image16 from "./assets/images/16.jpg";
+import Image17 from "./assets/images/17.jpg";
+import Image18 from "./assets/images/18.jpg";
+import Image19 from "./assets/images/19.jpg";
+import Image20 from "./assets/images/20.jpg";
+import Image21 from "./assets/images/21.jpg";
+import Image22 from "./assets/images/22.jpg";
+import Image23 from "./assets/images/23.jpg";
+import Image24 from "./assets/images/24.jpg";
+import Image25 from "./assets/images/25.jpg";
+import Image26 from "./assets/images/26.jpg";
+import Image27 from "./assets/images/27.jpg";
+import Image28 from "./assets/images/28.jpg";
+import Image29 from "./assets/images/29.jpg";
+import Image30 from "./assets/images/30.jpg";
+import Image31 from "./assets/images/31.jpg";
+import Image32 from "./assets/images/32.jpg";
+import Image33 from "./assets/images/33.jpg";
+import Image34 from "./assets/images/34.jpg";
+import Image35 from "./assets/images/35.jpg";
+import Image36 from "./assets/images/36.jpg";
+import Image37 from "./assets/images/37.jpg";
+import Image38 from "./assets/images/38.jpg";
+import Image39 from "./assets/images/39.jpg";
+import Image40 from "./assets/images/40.jpg";
+import Image41 from "./assets/images/41.jpg";
+import Image42 from "./assets/images/42.jpg";
+import Image43 from "./assets/images/43.jpg";
+import Image44 from "./assets/images/44.jpg";
+import Image45 from "./assets/images/45.jpg";
+import Image46 from "./assets/images/46.jpg";
+import Image47 from "./assets/images/47.jpeg";
+import Image48 from "./assets/images/48.jpeg";
+import Image49 from "./assets/images/49.jpeg";
+import Image50 from "./assets/images/50.jpeg";
+import Image51 from "./assets/images/51.jpeg";
+import Image52 from "./assets/images/52.jpeg";
+import Image53 from "./assets/images/53.jpeg";
+import Image54 from "./assets/images/54.jpeg";
+import Image55 from "./assets/images/55.jpeg";
+import Image56 from "./assets/images/56.jpeg";
+import Image57 from "./assets/images/57.jpeg";
+import Image58 from "./assets/images/58.jpeg";
+import Image59 from "./assets/images/59.jpeg";
+import Image60 from "./assets/images/60.jpeg";
+
+const images = [
+  {
+    id: 1,
+    img: Image01,
+    name: "Image 01",
+  },
+  {
+    id: 2,
+    img: Image02,
+    name: "Image 02",
+  },
+  {
+    id: 3,
+    img: Image03,
+    name: "Image 03",
+  },
+  {
+    id: 4,
+    img: Image04,
+    name: "Image 04",
+  },
+  {
+    id: 5,
+    img: Image05,
+    name: "Image 05",
+  },
+  {
+    id: 6,
+    img: Image06,
+    name: "Image 06",
+  },
+  {
+    id: 7,
+    img: Image07,
+    name: "Image 07",
+  },
+  {
+    id: 8,
+    img: Image08,
+    name: "Image 08",
+  },
+  {
+    id: 9,
+    img: Image09,
+    name: "Image 09",
+  },
+  {
+    id: 10,
+    img: Image10,
+    name: "Image 10",
+  },
+  {
+    id: 11,
+    img: Image11,
+    name: "Image 11",
+  },
+  {
+    id: 12,
+    img: Image12,
+    name: "Image 12",
+  },
+  {
+    id: 13,
+    img: Image13,
+    name: "Image 13",
+  },
+  {
+    id: 14,
+    img: Image14,
+    name: "Image 14",
+  },
+  {
+    id: 15,
+    img: Image15,
+    name: "Image 15",
+  },
+  {
+    id: 16,
+    img: Image16,
+    name: "Image 16",
+  },
+  {
+    id: 17,
+    img: Image17,
+    name: "Image 17",
+  },
+  {
+    id: 18,
+    img: Image18,
+    name: "Image 18",
+  },
+  {
+    id: 19,
+    img: Image19,
+    name: "Image 19",
+  },
+  {
+    id: 20,
+    img: Image20,
+    name: "Image 20",
+  },
+  {
+    id: 21,
+    img: Image21,
+    name: "Image 21",
+  },
+  {
+    id: 22,
+    img: Image22,
+    name: "Image 22",
+  },
+  {
+    id: 23,
+    img: Image23,
+    name: "Image 23",
+  },
+  {
+    id: 24,
+    img: Image24,
+    name: "Image 24",
+  },
+  {
+    id: 25,
+    img: Image25,
+    name: "Image 25",
+  },
+  {
+    id: 26,
+    img: Image26,
+    name: "Image 26",
+  },
+  {
+    id: 27,
+    img: Image27,
+    name: "Image 27",
+  },
+  {
+    id: 28,
+    img: Image28,
+    name: "Image 28",
+  },
+  {
+    id: 29,
+    img: Image29,
+    name: "Image 29",
+  },
+  {
+    id: 30,
+    img: Image30,
+    name: "Image 30",
+  },
+  {
+    id: 31,
+    img: Image31,
+    name: "Image 31",
+  },
+  {
+    id: 32,
+    img: Image32,
+    name: "Image 32",
+  },
+  {
+    id: 33,
+    img: Image33,
+    name: "Image 33",
+  },
+  {
+    id: 34,
+    img: Image34,
+    name: "Image 34",
+  },
+  {
+    id: 35,
+    img: Image35,
+    name: "Image 35",
+  },
+  {
+    id: 36,
+    img: Image36,
+    name: "Image 36",
+  },
+  {
+    id: 37,
+    img: Image37,
+    name: "Image 37",
+  },
+  {
+    id: 38,
+    img: Image38,
+    name: "Image 38",
+  },
+  {
+    id: 39,
+    img: Image39,
+    name: "Image 39",
+  },
+  {
+    id: 40,
+    img: Image40,
+    name: "Image 40",
+  },
+  {
+    id: 41,
+    img: Image41,
+    name: "Image 41",
+  },
+  {
+    id: 42,
+    img: Image42,
+    name: "Image 42",
+  },
+  {
+    id: 43,
+    img: Image43,
+    name: "Image 43",
+  },
+  {
+    id: 44,
+    img: Image44,
+    name: "Image 44",
+  },
+  {
+    id: 45,
+    img: Image45,
+    name: "Image 45",
+  },
+  {
+    id: 46,
+    img: Image46,
+    name: "Image 46",
+  },
+  {
+    id: 47,
+    img: Image47,
+    name: "Image 47",
+  },
+  {
+    id: 48,
+    img: Image48,
+    name: "Image 48",
+  },
+  {
+    id: 49,
+    img: Image49,
+    name: "Image 49",
+  },
+  {
+    id: 50,
+    img: Image50,
+    name: "Image 50",
+  },
+  {
+    id: 51,
+    img: Image51,
+    name: "Image 51",
+  },
+  {
+    id: 52,
+    img: Image52,
+    name: "Image 52",
+  },
+  {
+    id: 53,
+    img: Image53,
+    name: "Image 53",
+  },
+  {
+    id: 54,
+    img: Image54,
+    name: "Image 54",
+  },
+  {
+    id: 55,
+    img: Image55,
+    name: "Image 55",
+  },
+  {
+    id: 56,
+    img: Image56,
+    name: "Image 56",
+  },
+  {
+    id: 57,
+    img: Image57,
+    name: "Image 57",
+  },
+  {
+    id: 58,
+    img: Image58,
+    name: "Image 58",
+  },
+  {
+    id: 59,
+    img: Image59,
+    name: "Image 59",
+  },
+  {
+    id: 60,
+    img: Image60,
+    name: "Image 60",
+  },
+];
+
 function App() {
-  return <div>App</div>
+  return (
+    <div className="products">
+      {images.map((image) => (
+        <article className="products__card" key={image.id}>
+          <img src={image.img} alt={image.name} />
+          <p>{image.name}</p>
+        </article>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
